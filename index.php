ea175357b32d94c2720d8525230678ab624b9f6e
YusufAlfiansyah <46749583+YusufAlfiansyah@users.noreply.github.com>
2019-01-16 21:25:19 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<table width="337">
+
+<td width="298" height="300" align="center"> <p><strong>Nama : Muh Yusuf Alfiansya</strong></p>
+  <p><strong> Nim : 215 280 005</strong></p>
+  <p><strong>Phone: 085 145 *** ***</strong></p>
+  <p><strong>Fax: ricardoyhusuf@gmail.com</strong></p>
+  <p><strong>Sulawesi Selatan</strong></p>
+ </table>
